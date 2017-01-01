41053fba3a031b8c6fcbaeedf8d35f9f5201ca46
Theo <theo.a.sherman@gmail.com>
2017-01-01 13:27:57 -0600

    added links to vue.js and webpack.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,9 @@
 <template lang="pug">
 div
   h1 Home Sweet Home
-  h3.text-muted This demo will demonstrate the power of Vue.js
+  h3.text-muted
+    | This demo will demonstrate the power of&nbsp;
+    a(href="https://vuejs.org/") Vue.js
+    | &nbsp;and&nbsp;
+    a(href="https://webpack.github.io/") Webpack
 </template>
-
-<script>
-export default {
-}
-</script>
-
-<style lang="css">
-</style>
