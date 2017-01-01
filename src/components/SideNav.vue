8b2bcf4520aef457109cdd1b581a8d788409e925
Theo <theo.a.sherman@gmail.com>
2017-01-01 14:06:27 -0600

    removed unnecessary sections side nav.

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,15 +1,7 @@
 <template lang="pug">
-
 .list-group
   router-link.list-group-item(to="/" exact) Home
   router-link.list-group-item(to="/databinding") Data Binding
   router-link.list-group-item(to="/grid") Grid
   router-link.list-group-item(to="/form") Form
-
 </template>
-
-<script>
-export default {
-  name: 'SideNav'
-}
-</script>
