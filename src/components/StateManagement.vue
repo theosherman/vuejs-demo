9c18d8dc1f77d54690a2b2a44a8d253c06e4c197
Theo Sherman <theo.a.sherman@gmail.com>
2017-02-07 21:39:09 -0600

    altered layout of state management component.

--- src/components/StateManagement.vue
+++ src/components/StateManagement.vue
@@ -2,14 +2,18 @@
 div
   h1 State Management
   hr
-  pre {{ $store.state }}
+  .row
 
-  .input-group
-    input.form-control(v-model.trim="nameInput", @keyup.enter="setDisplayName(nameInput)", placeholder = "displayName")
-    .input-group-btn
-      button.btn.btn-success(@click="setDisplayName(nameInput)")
-        i.fa.fa-save
-        | &nbsp;Save
+    .col-md-6
+      .input-group
+        input.form-control(v-model.trim="nameInput", @keyup.enter="setDisplayName(nameInput)", placeholder = "displayName")
+        .input-group-btn
+          button.btn.btn-success(@click="setDisplayName(nameInput)")
+            i.fa.fa-save
+            | &nbsp;Save
+
+    .col-md-6
+      pre {{ $store.state }}
 </template>
 
 <script>
